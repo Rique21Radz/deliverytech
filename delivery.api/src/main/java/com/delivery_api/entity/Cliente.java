abd60e400c58e0ba44b2b58363728a72714d8356
Henrique Radzevicius Toledo <rique21radz@gmail>
2025-08-21 16:55:46 -0300

    Entidades do projeto

---
+++ delivery.api/src/main/java/com/delivery_api/entity/Cliente.java
@@ -0,0 +1,44 @@
+package com.delivery_api.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private boolean ativo;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos;
+
+    public Cliente(String nome, String email, String telefone, String endereco, boolean ativo) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.ativo = ativo;
+    }
+
+    // Método utilitário para inativação.
+    public void inativar() {
+        this.ativo = false;
+    }
+    
+}
